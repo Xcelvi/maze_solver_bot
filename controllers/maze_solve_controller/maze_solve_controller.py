df53959ee188cc05a0ca7d630cc7fa300af38e09
XcelVI <keatonasuren@gmail.com>
2026-07-14 12:32:38 -0500

    Added distance from right wall tracking, as well as turn aggression to keep certain distances apart

--- controllers/maze_solve_controller/maze_solve_controller.py
+++ controllers/maze_solve_controller/maze_solve_controller.py
@@ -36,7 +36,7 @@ right_motor = robot.getDevice('right wheel motor')
 left_motor.setPosition(float('inf'))
 right_motor.setPosition(float('inf'))
 #set max speed
-max_speed = 6.28
+max_speed = 4
 
 #set the motors to go
 left_motor.setVelocity(max_speed)
@@ -44,6 +44,14 @@ right_motor.setVelocity(max_speed)
 
 left_speed = max_speed
 right_speed = max_speed
+
+# Add a distance you want the robot from the wall
+target_distance = 150
+turn_aggression = .04
+
+#Set max velo to not break
+motor_limit = 6.28
+
 while robot.step(timestep) != -1:
     
     forward0 = int(ps0.getValue())
@@ -59,26 +67,32 @@ while robot.step(timestep) != -1:
     left0 = int(ps5.getValue())
     left1 = int(ps6.getValue())
     
-    right_sensors = [right0, right1]
-    forward_sensors = [forward0, forward1, forward2]
-    left_sensors = [left0, left1]
-    back_sensors = [back0, back1]
+    right_sensor = max(right0, right1)
+    forward_sensor = max(forward0, forward1, forward2)
+    left_sensor = max(left0, left1)
+    back_sensor = max(back0, back1)
     
-    # Process sensor data here.
-    if (max(right_sensors) < 75):
-        left_speed = max_speed
-        right_speed = -4
-        print("right")
-    elif (max(forward_sensors) < 75):
-        right_speed = max_speed
-        left_speed = max_speed
+    # Turning amount and erros
+    if forward_sensor > 200:
+        left_motor.setVelocity(-4)
+        right_motor.setVelocity(4)
+        for i in range(30):
+            robot.step(timestep)
         print("forward")
-    else: 
-        right_speed = max_speed
-        left_speed = -4
-        print("left")
+    else:    
+        distance_from_ideal = target_distance - right_sensor
+        if (abs(distance_from_ideal) < 80):
+            distance_from_ideal = 0
+        turn_correction = distance_from_ideal * turn_aggression
+        
+        left_speed = max_speed + turn_correction
+        right_speed = max_speed - turn_correction
+        
+        left_speed = max(min(left_speed, motor_limit), -motor_limit)
+        right_speed = max(min(right_speed, motor_limit), -motor_limit)
+        print("Right", right_sensor, "left", left_sensor, "distance_from_ideal", distance_from_ideal)
     left_motor.setVelocity(left_speed)
-    right_motor.setVelocity(right_speed)    
+    right_motor.setVelocity(right_speed)
     
     pass
 
